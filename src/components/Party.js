5135581af35be44ecb33de1f8fea8a66949b3196
Ellinor Carlstedt <ellinor.carlstedt@gmail.com>
2018-09-04 16:45:00 +0200

    created party component

---
+++ src/components/Party.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Party extends Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello Party!</h1>
+            </div>
+        );
+    }
+}
+
+export default Party;
